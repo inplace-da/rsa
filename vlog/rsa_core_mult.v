cd722b2de0ed4f740f962ffd629192e1231b3e2c
Cid <alcides@inplace-da.com.br>
2025-03-14 22:59:46 -0300

    Fixed several translations errors from VHDL to Verilog

--- vlog/rsa_core_mult.v
+++ vlog/rsa_core_mult.v
@@ -14,97 +14,77 @@ module rsa_core_mult #(
   output                      mult_done,
   output [2*DATA_WIDTH-1:0]   mult_c
 );
-
-  // Manual calculation of the number of bits required for a_cnt
-  localparam BIT_WIDTH =
-   (DATA_WIDTH == 1) ? 1:
-   (DATA_WIDTH <= 2) ? 1:
-   (DATA_WIDTH <= 4) ? 2:
-   (DATA_WIDTH <= 8) ? 3:
-   (DATA_WIDTH <= 16) ? 4:
-   (DATA_WIDTH <= 32) ? 5:6;
-
   // Explicitly size the state constants as 3-bit numbers
-  localparam [2:0] INIT      = 3'd0,
-                   ANALYZE   = 3'd1,
-                   SHIFT_ADD = 3'd2,
-                   SHIFT     = 3'd3,
-                   DONE      = 3'd4;
+  	localparam [2:0]INIT      = 3'd0,
+                  	ANALYZE   = 3'd1,
+                   	SHIFT_ADD = 3'd2,
+                   	SHIFT     = 3'd3,
+                   	DONE      = 3'd4;
 
-  reg [2:0] state_reg, state_ns;
-  reg [BIT_WIDTH-1:0] a_cnt;
-  reg [DATA_WIDTH-1:0] a_reg, b_reg;
-  reg [2*DATA_WIDTH-1:0] p_reg, c_reg;
-  reg done_ff;
+ 	reg [2:0] state_reg, state_ns;
+  	reg [$clog2(DATA_WIDTH+1)-1:0] a_cnt;
+  	reg [DATA_WIDTH-1:0] a_reg, b_reg;
+ 	reg [2*DATA_WIDTH-1:0] p_reg, c_reg;
+  	reg done_ff;
 
-  wire actual_clk = CLK_EDGE ? mult_clk : ~mult_clk;
+	// OUTPUT SIGNALS CONNECTIONS
+   	assign mult_done = done_ff;
+  	assign mult_c    = c_reg;
 
-  assign mult_done = done_ff;
-  assign mult_c    = c_reg;
+	// NEXT STATE DECODE LOGIC
+	always @ (mult_rst, mult_start, b_reg[7], a_cnt, state_reg) begin
+		if (mult_rst == RESET)
+	    	state_ns = INIT;
+	    else begin
+	     	case(state_reg)
+		        INIT:
+		        	state_ns = (mult_start == START) ? ANALYZE : INIT;
+		        ANALYZE: state_ns = (b_reg[DATA_WIDTH-1]) ? SHIFT_ADD : SHIFT;
+		        SHIFT_ADD: begin
+		          if (a_cnt != (DATA_WIDTH-1))
+		            state_ns = (b_reg[DATA_WIDTH-1]) ? SHIFT_ADD : SHIFT;
+		          else
+		            state_ns = DONE;
+		        end
+		        SHIFT: begin
+		          if (a_cnt != (DATA_WIDTH-1))
+		            state_ns = (b_reg[DATA_WIDTH-1]) ? SHIFT_ADD : SHIFT;
+		          else
+		            state_ns = DONE;
+		        end
+		        DONE: state_ns = INIT;
+		        default: state_ns = INIT;
+	    	endcase
+	    end
+	  end
 
-  always @* begin
-    if (mult_rst == RESET) begin
-      state_ns = INIT;
-    end else begin
-      case(state_reg)
-        INIT: state_ns = (mult_start == START) ? ANALYZE : INIT;
-        ANALYZE: state_ns = (b_reg[DATA_WIDTH-1]) ? SHIFT_ADD : SHIFT;
-        SHIFT_ADD: begin
-          if (a_cnt != (DATA_WIDTH-1))
-            state_ns = (b_reg[DATA_WIDTH-1]) ? SHIFT_ADD : SHIFT;
-          else
-            state_ns = DONE;
-        end
-        SHIFT: begin
-          if (a_cnt != (DATA_WIDTH-1))
-            state_ns = (b_reg[DATA_WIDTH-1]) ? SHIFT_ADD : SHIFT;
-          else
-            state_ns = DONE;
-        end
-        DONE: state_ns = INIT;
-        default: state_ns = INIT;
-      endcase
-    end
-  end
-
-  always @(posedge actual_clk) begin
-    if (mult_rst == RESET) begin
-      state_reg <= INIT;
-      p_reg     <= 0;
-      a_cnt     <= 0;
-      done_ff   <= 0;
-      a_reg     <= 0;
-      b_reg     <= 0;
-      c_reg     <= 0;
-    end else begin
-      state_reg <= state_ns;
-      case(state_reg)
-        INIT: begin
-          p_reg   <= 0;
-          a_cnt   <= 0;
-          done_ff <= 0;
-          a_reg   <= mult_a;
-          b_reg   <= mult_b;
-        end
-        ANALYZE: begin
-          b_reg <= {b_reg[DATA_WIDTH-2:0], 1'b0};
-        end
-        SHIFT_ADD: begin
-          p_reg <= a_reg + {p_reg[2*DATA_WIDTH-2:0], 1'b0};
-          a_cnt <= a_cnt + 1'b1;
-          b_reg <= {b_reg[DATA_WIDTH-2:0], 1'b0};
-        end
-        SHIFT: begin
-          p_reg <= {p_reg[2*DATA_WIDTH-2:0], 1'b0};
-          a_cnt <= a_cnt + 1'b1;
-          b_reg <= {b_reg[DATA_WIDTH-2:0], 1'b0};
-        end
-        DONE: begin
-          done_ff <= 1'b1;
-          c_reg   <= p_reg;
-        end
-      endcase
-    end
-  end
-
+	always @(posedge mult_clk) begin
+		state_reg <= state_ns;
+    	case(state_reg)
+        	INIT: begin
+				p_reg   <= 0;
+				a_cnt   <= 0;
+				done_ff <= 1'b0;
+				a_reg   <= mult_a;
+				b_reg   <= mult_b;
+        	end
+			ANALYZE: begin
+				b_reg <= {b_reg[DATA_WIDTH-2:0], 1'b0};
+			end
+			SHIFT_ADD: begin
+				p_reg <= a_reg + {p_reg[2*DATA_WIDTH-2:0], 1'b0};
+				a_cnt <= a_cnt + 1'b1;
+				b_reg <= {b_reg[DATA_WIDTH-2:0], 1'b0};
+			end
+			SHIFT: begin
+				p_reg <= {p_reg[2*DATA_WIDTH-2:0], 1'b0};
+				a_cnt <= a_cnt + 1'b1;
+				b_reg <= {b_reg[DATA_WIDTH-2:0], 1'b0};
+			end
+			DONE: begin
+				done_ff <= 1'b1;
+				c_reg   <= p_reg;
+			end
+		endcase
+	end
 endmodule
